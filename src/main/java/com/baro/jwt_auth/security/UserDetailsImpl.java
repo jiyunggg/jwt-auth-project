b976ce935bb81ac0661cff9b6b170032dc002af6
jiyun <jiyyy0720@naver.com>
2025-07-06 02:38:33 +0900

    feat: userDetailsServiceImpl 구현 (UserEntity 권한/정보 매핑)

---
+++ src/main/java/com/baro/jwt_auth/security/UserDetailsImpl.java
@@ -0,0 +1,63 @@
+package com.baro.jwt_auth.security;
+
+import com.baro.jwt_auth.user.entity.UserEntity;
+import com.baro.jwt_auth.user.entity.UserRoleEnum;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private final UserEntity user;
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    // 권한 정보를 GrantedAuthority 컬렉션으로 변환
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRoleEnum role = user.getRole();
+        String authority = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority); // 시큐리티 권한 객체
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
