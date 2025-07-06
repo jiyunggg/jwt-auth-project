9f9c997b60991b169977f2a031f829eddcd35a4f
jiyun <jiyyy0720@naver.com>
2025-07-06 17:12:37 +0900

    feat: 스웨거 어노테이션 추가(요청 예시)

--- src/main/java/com/baro/jwt_auth/user/dto/request/LoginRequestDto.java
+++ src/main/java/com/baro/jwt_auth/user/dto/request/LoginRequestDto.java
@@ -1,5 +1,6 @@
 package com.baro.jwt_auth.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,9 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequestDto {
+    @Schema(description = "사용자 ID", example = "aaa")
     @NotBlank(message = "아이디를 입력해주세요.")
     private String username;
 
+    @Schema(description = "사용자 비밀번호", example = "aaa123")
     @NotBlank(message = "비밀번호를 입력해주세요.")
     private String password;
 }
